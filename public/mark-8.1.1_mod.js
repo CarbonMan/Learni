4be4740a4f1964ef868e1bdffd87cfce32a8a4d6
CarbonMan <CarbonMan@users.noreply.github.com>
2021-08-03 15:23:47 +1000

    Update and rename mark-8.1.1.js to mark-8.1.1_mod.js

--- public/mark-8.1.1_mod.js
+++ public/mark-8.1.1_mod.js
@@ -7,7 +7,7 @@
 
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
+	/* typeof define === 'function' && define.amd ? define(factory) */ false :
 	(global.Mark = factory());
 }(this, (function () { 'use strict';
 
