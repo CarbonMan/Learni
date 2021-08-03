b5990d79f997c703ddd3be69b8d432fc30df466d
CarbonMan <CarbonMan@users.noreply.github.com>
2021-08-03 16:10:50 +1000

    Update mark-8.1.1_mod.js

--- public/mark-8.1.1_mod.js
+++ public/mark-8.1.1_mod.js
@@ -7,7 +7,7 @@
 
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	/* typeof define === 'function' && define.amd ? define(factory) */ false :
+	/* typeof define === 'function' && define.amd ? define(factory) : */
 	(global.Mark = factory());
 }(this, (function () { 'use strict';
 
